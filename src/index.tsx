651e478a06db3cbe68c7426feebdaf05291e53ea
Bill Story <billstory@MacBook-Pro.fios-router.home>
2019-10-29 17:15:17 -0400

    remove notes

--- src/index.tsx
+++ src/index.tsx
@@ -13,8 +13,6 @@ import Pages from './pages';
 import Login from './pages/Login';
 import { typeDefs } from './resolvers'
 
-// Set up our apollo-client to point at the server we created
-// this can be local or a remote endpoint
 const cache = new InMemoryCache();
 const client = new ApolloClient({
   cache,
@@ -34,16 +32,6 @@ cache.writeData({
     cartItems: [],
   },
 });
-
-/**
- * Render our app
- * - We wrap the whole app with ApolloProvider, so any component in the app can
- *    make GraphqL requests. Our provider needs the client we created above,
- *    so we pass it as a prop
- * - We need a router, so we can navigate the app. We're using Reach router for this.
- *    The router chooses between which component to render, depending on the url path.
- *    ex: localhost:3000/login will render only the `Login` component
- */
 
 const IS_LOGGED_IN = gql`
   query IsUserLoggedIn {
